99766ab8bed385a9811cfa9689d94140cd19b9b3
João Victor <victorps.joao@gmail.com>
2021-01-06 15:42:24 -0300

    Insure landing page

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frontend Mentor - Insure landing page
+
+![Design preview for the Insure landing page coding challenge](./design/desktop-preview.jpg)
---
+++ script.js
@@ -0,0 +1,4 @@
+const handleMenu = () => {
+    let menu = document.getElementById('menu');
+    menu.classList.contains('none') ? menu.classList.remove('none') : menu.classList.add('none'); 
+}
